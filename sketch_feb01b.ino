32eb56f3773377eb955304adcfae2a953c6fede0
noSerious217 <gidrolisk314@yandex.ru>
2017-02-01 21:30:22 +0400

    Interrupt added

---
+++ sketch_feb01b.ino
@@ -0,0 +1,60 @@
+#include <Vehicle.h>
+
+Vehicle vh;
+
+void setup() {
+  vh.init(24,29);
+  attachInterrupt(0,interrupt0,RISING);
+  attachInterrupt(1,interrupt1,RISING);
+  attachInterrupt(2,interrupt2,RISING);
+  attachInterrupt(3,interrupt3,RISING);
+  attachInterrupt(4,interrupt4,RISING);
+  attachInterrupt(5,interrupt5,RISING);
+}
+
+void interrupt0()
+{
+  vh.backward();
+  delay(10);
+  vh.stop();
+}
+
+void interrupt1()
+{
+  vh.forward();
+  delay(10);
+  vh.stop();
+}
+
+void interrupt2()
+{
+  vh.againstclock();
+  delay(100);
+  interrupt0();
+}
+
+void interrupt3()
+{
+  vh.clock();
+  delay(100);
+  interrupt(0);
+}
+
+void interrupt4()
+{
+  vh.clock();
+  delay(100);
+  interrupt1();
+}
+
+void interrupt5()
+{
+  vh.againstclock();
+  delay(100);
+  interrupt1();
+}
+
+void loop() {
+  
+ }
+
